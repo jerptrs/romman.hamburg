b09b6e82828ffc72871e247e7e1539a2f28a3170
jerptrs <jerptrs@gmail.com>
2020-10-13 21:15:19 +0200

    'iunno

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -4,11 +4,40 @@ import { ResponsiveHelper } from '../../common/responsive-helper';
 import { Title } from '@angular/platform-browser';
 import { environment } from '@src/environments/environment';
 import {faPhone, faEnvelope, faClock, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
-
+import { trigger, transition, style, animate, state } from '@angular/animations';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({transform: 'translateX(-150%)'}),
+        animate('800ms ease-in', style({transform: 'translateX(0%)'}))
+      ]),
+      transition(':leave', [
+        animate('800ms ease-in', style({transform: 'translateX(-100%)'}))
+      ])
+    ]),
+    trigger('fadeIn', [
+      state('inactive', style({
+        opacity: 0,
+      })),
+      transition('inactive => active', [ // using status here for transition
+        style({
+          opacity: 0
+        }),
+        animate('1.25s cubic-bezier(.5, .4, .2, 0)', style({
+          opacity: 1
+        }))
+      ]),
+      transition('active => inactive', [
+        animate('1.25s cubic-bezier(.5, .4, .2, 0)', style({
+          opacity: 0
+        }))
+      ])
+    ])
+  ]
 })
 export class ContactComponent implements OnInit {
   phoneIcon = faPhone;
@@ -26,7 +55,7 @@ export class ContactComponent implements OnInit {
       validators: [Validators.required]
     }]
   })
-
+  animationDelay = 'inactive';
   isHandset = this.responsiveHelper.isHandset;
 
   appName: string;
@@ -37,6 +66,9 @@ export class ContactComponent implements OnInit {
 
   ngOnInit(): void {
     this.title.setTitle(`Contact - ${this.appName}`);
+    setTimeout(() => {
+      this.animationDelay = 'active';
+    }, 800);
   }
 
   submitForm(): void {
--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -2,15 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { ResponsiveHelper } from '../../common/responsive-helper';
 import { Title } from '@angular/platform-browser';
 import { environment } from '@src/environments/environment';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      state('inactive', style({
+        opacity: 0,
+      })),
+      transition('inactive => active', [ // using status here for transition
+        style({
+          opacity: 0
+        }),
+        animate('1.25s cubic-bezier(.5, .4, .2, 0)', style({
+          opacity: 1
+        }))
+      ]),
+      transition('active => inactive', [
+        animate('1.25s cubic-bezier(.5, .4, .2, 0)', style({
+          opacity: 0
+        }))
+      ])
+    ])
+  ]
 })
 export class MenuComponent implements OnInit {
   appName: string;
-
+  animationDelay = 'inactive';
   constructor(private responsiveHelper: ResponsiveHelper, private title: Title) {
     this.appName = environment.name;
   }
@@ -19,6 +40,9 @@ export class MenuComponent implements OnInit {
 
   ngOnInit(): void {
     this.title.setTitle(`Menu - ${this.appName}`);
+    setTimeout(() => {
+      this.animationDelay = 'active';
+    }, 800);
   }
 
 }
